c31ba3a04deba288d4fad556f85c449b8a6ab596
hakim <ahmadhakim404@gmail.com>
2021-11-07 11:53:44 +0700

    merancang tabel database dengan migration

---
+++ app/Location.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    /**
+     * agar seluruh field di dalam tabel bisa diisi
+     *
+     * @var Array
+     */
+    protected $guarded = [];
+}
