ff48a1b2c47196e8534d8639df0c9570673d44f3
hakim <ahmadhakim404@gmail.com>
2021-11-08 11:34:33 +0700

    menampilkan peta leaflet di Laravel

--- config/leaflet.php
+++ config/leaflet.php
@@ -2,8 +2,6 @@
 return [
     'zoom_level'           => 13,
     'detail_zoom_level'    => 20,
-    'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '-0.498696490052'),
-    'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '117.15355485677'),
-    // UNTUK MENGAMBIL LATITUDE DAN LONGITUDE SILAHKAN LAKUKAN SECARA MANUAL ATAU AKSES HALAMAN CREATE KEMUDIAN
-    // GESER KE LOKASI YANG DI INGINKAN
+    'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '-6.905977'),
+    'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '107.613144'),
 ];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,17 +7,30 @@
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                <div class="card-body" id="mapid">
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+@push('styles')
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+<style>
+    #mapid { min-height: 500px; }
+</style>
+@endpush
+
+@push('scripts')
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+<script>
+    var map = L.map('mapid').setView([{{ config('leaflet.map_center_latitude') }},
+        {{ config('leaflet.map_center_longitude') }}],
+        {{ config('leaflet.zoom_level') }});
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    }).addTo(map);
+</script>
+@endpush
